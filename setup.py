6f9716839d5e5e37ea2a1aeb11e9c93aca3f3555
wndhydrnt <hydrantanderwand@gmail.com>
2013-10-12 22:24:16 +0200

    Added Python 3.3 to setup.py and build file

--- setup.py
+++ setup.py
@@ -18,5 +18,6 @@ setup(name="python-oauth2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
     ]
 )
