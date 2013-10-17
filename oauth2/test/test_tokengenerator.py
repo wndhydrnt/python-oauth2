c7c59268215ea9f92162619c241714b2d517fee2
wndhydrnt <hydrantanderwand@gmail.com>
2013-10-17 20:10:22 +0200

    Added test of Uuid4 token generator

--- oauth2/test/test_tokengenerator.py
+++ oauth2/test/test_tokengenerator.py
@@ -1,5 +1,6 @@
+import re
 from oauth2.test import unittest
-from oauth2.tokengenerator import URandomTokenGenerator
+from oauth2.tokengenerator import URandomTokenGenerator, Uuid4
 
 class URandomTokenGeneratorTestCase(unittest.TestCase):
     def test_generate(self):
@@ -11,6 +12,18 @@ class URandomTokenGeneratorTestCase(unittest.TestCase):
         
         self.assertTrue(isinstance(result, str))
         self.assertEqual(len(result), length)
+
+class Uuid4TestCase(unittest.TestCase):
+    def test_generate(self):
+        generator = Uuid4()
+        
+        result = generator.generate()
+        
+        regex = re.compile(r"^[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}-[a-z0-9]{12}$")
+        
+        match = regex.match(result)
+        
+        self.assertEqual(result, match.group())
 
 if __name__ == "__main__":
     unittest.main()
