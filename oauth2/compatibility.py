0940d55e522301be2849d5fe8950acce342ebc20
wndhydrnt <hydrantanderwand@gmail.com>
2013-10-20 15:27:11 +0200

    Ignore lines in code coverage run

--- oauth2/compatibility.py
+++ oauth2/compatibility.py
@@ -5,10 +5,10 @@ Ensures compatibility between python 2.x and python 3.x
 import sys
  
 if sys.version_info >= (3, 0):
-    from urllib.parse import parse_qs as parse_qs
-    from urllib.parse import urlencode as urlencode
-    from urllib.parse import quote as quote
+    from urllib.parse import parse_qs # pragma: no cover
+    from urllib.parse import urlencode # pragma: no cover
+    from urllib.parse import quote # pragma: no cover
 else:
-    from urlparse import parse_qs as parse_qs
-    from urllib import urlencode as urlencode
-    from urllib import quote as quote
+    from urlparse import parse_qs # pragma: no cover
+    from urllib import urlencode # pragma: no cover
+    from urllib import quote # pragma: no cover
