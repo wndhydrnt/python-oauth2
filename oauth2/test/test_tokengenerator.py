772e155fa42ed2ed0981751b624684faeb13e360
wndhydrnt <hydrantanderwand@gmail.com>
2013-11-26 19:18:21 +0100

    Create access token data in token generators

--- oauth2/test/test_tokengenerator.py
+++ oauth2/test/test_tokengenerator.py
@@ -14,12 +14,33 @@ class URandomTokenGeneratorTestCase(unittest.TestCase):
         self.assertEqual(len(result), length)
 
 class Uuid4TestCase(unittest.TestCase):
+    def setUp(self):
+        self.uuid_regex = r"^[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}-[a-z0-9]{12}$"
+    
+    def test_create_access_token_data_no_expiration(self):
+        generator = Uuid4()
+        
+        result = generator.create_access_token_data()
+        
+        self.assertRegexpMatches(result["access_token"], self.uuid_regex)
+        self.assertEqual(result["token_type"], "Bearer")
+    
+    def test_create_access_token_data_with_expiration(self):
+        generator = Uuid4(expires_in=600)
+        
+        result = generator.create_access_token_data()
+        
+        self.assertRegexpMatches(result["access_token"], self.uuid_regex)
+        self.assertEqual(result["token_type"], "Bearer")
+        self.assertRegexpMatches(result["refresh_token"], self.uuid_regex)
+        self.assertEqual(result["expires_in"], 600)
+    
     def test_generate(self):
         generator = Uuid4()
         
         result = generator.generate()
         
-        regex = re.compile(r"^[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}-[a-z0-9]{12}$")
+        regex = re.compile(self.uuid_regex)
         
         match = regex.match(result)
         
--- oauth2/tokengenerator.py
+++ oauth2/tokengenerator.py
@@ -1,19 +1,71 @@
+"""
+Provides various implementations of algorithms to generate a token.
+"""
+
 import hashlib
 import os
 import uuid
 
-class URandomTokenGenerator(object):
-    def __init__(self, length=40):
-        self.token_length = length
+class TokenGenerator(object):
+    """
+    Base class of every token generator.
+    """
+    def __init__(self, expires_in=0):
+        """
+        Create a new instance of a token generator.
+        
+        :param expires_in: Timeframe in seconds that defines how long a
+                           generated token will be valid. Default: 0
+        """
+        self.expires_in = expires_in
+    
+    def create_access_token_data(self):
+        """
+        Create data needed by an access token.
+        
+        :return: A ``dict`` containing he ``access_token`` and the
+                 ``token_type``. If the value of ``TokenGenerator.expires_in``
+                 is larger than 0, a ``refresh_token`` will be generated too.
+        """
+        result = {"access_token": self.generate(), "token_type": "Bearer"}
+        
+        if self.expires_in > 0:
+            result["refresh_token"] = self.generate()
+            result["expires_in"] = self.expires_in
+        
+        return result
     
     def generate(self):
-        randomData = os.urandom(100)
+        """
+        Implemented by generators extending this base class.
+        """
+        raise NotImplementedError
+
+class URandomTokenGenerator(TokenGenerator):
+    """
+    Create a token using ``os.urandom()``.
+    """
+    def __init__(self, expires_in=0, length=40):
+        self.token_length = length
+        TokenGenerator.__init__(self, expires_in=expires_in)
+    
+    def generate(self):
+        """
+        Returns a new token.
+        """
+        random_data = os.urandom(100)
         
         hash_gen = hashlib.new("sha512")
-        hash_gen.update(randomData)
+        hash_gen.update(random_data)
         
         return hash_gen.hexdigest()[:self.token_length]
 
-class Uuid4(object):
+class Uuid4(TokenGenerator):
+    """
+    Generate a token using uuid4.
+    """
     def generate(self):
+        """
+        Returns a new token.
+        """
         return str(uuid.uuid4())
