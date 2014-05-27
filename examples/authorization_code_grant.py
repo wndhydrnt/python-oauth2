9dc57e9d490a3e51f57107cf54c7cd7470d1c556
wndhydrnt <hydrantanderwand@gmail.com>
2014-05-27 22:35:50 +0200

    Fix examples always redirecting to confirmation page

--- examples/authorization_code_grant.py
+++ examples/authorization_code_grant.py
@@ -20,7 +20,7 @@ from oauth2.grant import AuthorizationCodeGrant
 class ClientRequestHandler(WSGIRequestHandler):
     """
     Request handler that enables formatting of the log messages on the console.
-    
+
     This handler is used by the client application.
     """
     def address_string(self):
@@ -29,7 +29,7 @@ class ClientRequestHandler(WSGIRequestHandler):
 class OAuthRequestHandler(WSGIRequestHandler):
     """
     Request handler that enables formatting of the log messages on the console.
-    
+
     This handler is used by the python-oauth2 application.
     """
     def address_string(self):
@@ -40,7 +40,7 @@ class TestSiteAdapter(SiteAdapter):
     This adapter renders a confirmation page so the user can confirm the auth
     request.
     """
-    
+
     CONFIRMATION_TEMPLATE = """
 <html>
     <body>
@@ -59,21 +59,21 @@ class TestSiteAdapter(SiteAdapter):
     </body>
 </html>
     """
-    
+
     def render_auth_page(self, request, response, environ, scopes):
         response.body = self.CONFIRMATION_TEMPLATE
-        
+
         return response
-    
+
     def authenticate(self, request, environ, scopes):
         if request.method == "POST":
-            if request.post_param("confirm") is "1":
+            if request.post_param("confirm") == "1":
                 return
         raise UserNotAuthenticated
-    
+
     def user_has_denied_access(self, request):
         if request.method == "POST":
-            if request.post_param("confirm") is "0":
+            if request.post_param("confirm") == "0":
                 return True
         return False
 
@@ -86,12 +86,12 @@ class ClientApplication(object):
     client_id = "abc"
     client_secret = "xyz"
     api_server_url = "http://localhost:8080"
-    
+
     def __init__(self):
         self.access_token = None
         self.auth_token   = None
         self.token_type   = ""
-    
+
     def __call__(self, env, start_response):
         if env["PATH_INFO"] == "/app":
             status, body, headers = self._serve_application()
@@ -101,57 +101,57 @@ class ClientApplication(object):
             status = "301 Moved"
             body = ""
             headers = {"Location": "/app"}
-        
+
         start_response(status,
                        [(header, val) for header,val in headers.iteritems()])
         return body
-    
+
     def _request_access_token(self):
         print("Requesting access token...")
-        
+
         post_params = {"client_id": self.client_id,
                        "client_secret": self.client_secret,
                        "code": self.auth_token,
                        "grant_type": "authorization_code",
                        "redirect_uri": self.callback_url}
         token_endpoint = self.api_server_url + "/token"
-        
+
         result = urllib.urlopen(token_endpoint,
                                 urllib.urlencode(post_params))
         content = ""
         for line in result:
             content += line
-            
+
         result = json.loads(content)
         self.access_token = result["access_token"]
         self.token_type   = result["token_type"]
-        
+
         confirmation = "Received access token '%s' of type '%s'" % (self.access_token, self.token_type)
         print(confirmation)
         return "302 Found", "", {"Location": "/app"}
-    
+
     def _read_auth_token(self, env):
         print("Receiving authorization token...")
-        
+
         query_params = urlparse.parse_qs(env["QUERY_STRING"])
         self.auth_token = query_params["code"][0]
-        
+
         print("Received temporary authorization token '%s'" % (self.auth_token,))
-        
+
         return "302 Found", "", {"Location": "/app"}
-    
+
     def _request_auth_token(self):
         print("Requesting authorization token...")
-        
+
         auth_endpoint = self.api_server_url + "/authorize"
         query = urllib.urlencode({"client_id": "abc",
                                   "redirect_uri": self.callback_url,
                                   "response_type": "code"})
-        
+
         location = "%s?%s" % (auth_endpoint, query)
-        
+
         return "302 Found", "", {"Location": location}
-    
+
     def _serve_application(self):
         if self.access_token is None:
             if self.auth_token is None:
@@ -164,10 +164,10 @@ class ClientApplication(object):
 
 def run_app_server():
     app = ClientApplication()
-    
+
     try:
         httpd = make_server('', 8081, app, handler_class=ClientRequestHandler)
-    
+
         print("Starting Authorization Code Grant client app on http://localhost:8081/...")
         httpd.serve_forever()
     except KeyboardInterrupt:
@@ -178,9 +178,9 @@ def run_auth_server():
         client_store = ClientStore()
         client_store.add_client(client_id="abc", client_secret="xyz",
                                 redirect_uris=["http://localhost:8081/callback"])
-        
+
         token_store = TokenStore()
-        
+
         auth_controller = Provider(
             access_token_store=token_store,
             auth_code_store=token_store,
@@ -188,11 +188,11 @@ def run_auth_server():
             site_adapter=TestSiteAdapter(),
             token_generator=Uuid4())
         auth_controller.add_grant(AuthorizationCodeGrant())
-        
+
         app = Wsgi(server=auth_controller)
-        
+
         httpd = make_server('', 8080, app, handler_class=OAuthRequestHandler)
-    
+
         print("Starting implicit_grant oauth2 server on http://localhost:8080/...")
         httpd.serve_forever()
     except KeyboardInterrupt:
@@ -204,14 +204,14 @@ def main():
     app_server = Process(target=run_app_server)
     app_server.start()
     print("Access http://localhost:8081/app in your browser")
-    
+
     def sigint_handler(signal, frame):
         print("Terminating servers...")
         auth_server.terminate()
         auth_server.join()
         app_server.terminate()
         app_server.join()
-    
+
     signal.signal(signal.SIGINT, sigint_handler)
 
 if __name__ == "__main__":
--- examples/implicit_grant.py
+++ examples/implicit_grant.py
@@ -33,22 +33,22 @@ class TestSiteAdapter(SiteAdapter):
     </body>
 </html>
     """
-    
+
     def render_auth_page(self, request, response, environ, scopes):
         # Add check if the user is logged or a redirect to the login page here
         response.body = self.CONFIRMATION_TEMPLATE
-        
+
         return response
-    
+
     def authenticate(self, request, environ, scopes):
         if request.method == "POST":
-            if request.post_param("confirm") is "1":
+            if request.post_param("confirm") == "1":
                 return
         raise UserNotAuthenticated
-    
+
     def user_has_denied_access(self, request):
         if request.method == "POST":
-            if request.post_param("confirm") is "0":
+            if request.post_param("confirm") == "0":
                 return True
         return False
 
@@ -57,7 +57,7 @@ def run_app_server():
         """
         Serves the local javascript client
         """
-        
+
         js_app = """
 <html>
     <head>
@@ -68,18 +68,18 @@ def run_app_server():
         var accessToken = null;
         var params = {}
         var hash = window.location.hash.substring(1);
-        
+
         if (hash == "" && accessToken == null) {
             window.location.href = "http://localhost:8080/authorize?response_type=token&client_id=abc&redirect_uri=http%3A%2F%2Flocalhost%3A8081%2F&scope=scope_write"
         }
-        
+
         var hashParts = hash.split("&");
-        
+
         for (var i = 0; i < hashParts.length; i++) {
             var keyValue = hashParts[i].split("=");
             params[keyValue[0]] = keyValue[1]
         }
-        
+
         if ("access_token" in params) {
             alert("Your access token: " + params["access_token"]);
         } else {
@@ -95,14 +95,14 @@ def run_app_server():
     </body>
 </html>
         """
-        
+
         start_response("200 OK", [("Content-Type", "text/html")])
-        
+
         return [js_app]
 
     try:
         httpd = make_server('', 8081, application)
-    
+
         print("Starting implicit_grant app server on http://localhost:8081/...")
         httpd.serve_forever()
     except KeyboardInterrupt:
@@ -114,9 +114,9 @@ def run_auth_server():
         client_store = ClientStore()
         client_store.add_client(client_id="abc", client_secret="xyz",
                                 redirect_uris=["http://localhost:8081/"])
-        
+
         token_store = TokenStore()
-        
+
         auth_server = Provider(
             access_token_store=token_store,
             auth_code_store=token_store,
@@ -124,11 +124,11 @@ def run_auth_server():
             site_adapter=TestSiteAdapter(),
             token_generator=Uuid4())
         auth_server.add_grant(ImplicitGrant())
-        
+
         app = Wsgi(server=auth_server)
-        
+
         httpd = make_server('', 8080, app)
-    
+
         print("Starting implicit_grant oauth2 server on http://localhost:8080/...")
         httpd.serve_forever()
     except KeyboardInterrupt:
@@ -140,14 +140,14 @@ def main():
     app_server = Process(target=run_app_server)
     app_server.start()
     print("Access http://localhost:8081/ to start the auth flow")
-    
+
     def sigint_handler(signal, frame):
         print("Terminating servers...")
         auth_server.terminate()
         auth_server.join()
         app_server.terminate()
         app_server.join()
-    
+
     signal.signal(signal.SIGINT, sigint_handler)
 
 if __name__ == "__main__":
